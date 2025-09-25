529b0991060750a0941b2bac3a25147e13961633
janluyleonardo <janluy.moreno@gmail.com>
2025-09-25 02:34:25 -0500

    .

--- public/script.js
+++ public/script.js
@@ -4,11 +4,12 @@
 
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+console.log("Hello 🌎 JLMC");
 document.getElementById('formulario').addEventListener('submit', async (e) => {
     e.preventDefault();
     const nombre = document.getElementById('nombre').value;
     const comentario = document.getElementById('comentario').value;
+    console.log("nombre, comentario", nombre, comentario);
 
     const response = await fetch('/comentarios', {
         method: 'POST',
